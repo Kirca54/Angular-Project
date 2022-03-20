5077544a2b109ac83c8e3e67a03bba87274b2960
David Kirovski <41066264+Kirca54@users.noreply.github.com>
2022-03-20 22:23:53 +0100

    routing added

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -10,13 +10,11 @@ import {RecipeService} from "../../recipe.service";
 export class RecipeItemComponent implements OnInit {
 
   @Input() recipe: Recipe;
+  @Input() index: number;
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
   }
 
-  onSelected(){
-    this.recipeService.selectedRecipe.emit(this.recipe)
-  }
 
 }
