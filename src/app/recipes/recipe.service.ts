04ad24a6f8da5a830adf52b1e21345d8e7de78e1
David Kirovski <41066264+Kirca54@users.noreply.github.com>
2022-04-22 17:21:30 +0200

    added Firebase and Http req/res

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -5,40 +5,43 @@ import {ShoppingListService} from "../shopping-list/shopping-list.service";
 import {Subject} from "rxjs";
 
 @Injectable()
-export class RecipeService{
-
-
+export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  recipes:Recipe[]=[
-    new Recipe('Hambuger','Classic', 'https://s7d1.scene7.com/is/image/mcdo' +
-      'nalds/t-mcdonalds-Hamburger:1-3-product-tile-desktop?wid=829&hei=515&dpr=off',[
-        new Ingredient('buns',2),
-        new Ingredient('meat',1),
-        new Ingredient('ketchup',1),
-        new Ingredient('mayonnaise',1),
-        new Ingredient('pickle',2)
-    ]),
-    new Recipe('Cheeseburger','Classic','https://s7d1.scene7.com/is/image/m' +
-      'cdonalds/t-mcdonalds-Cheeseburger-1:1-3-product-tile-desktop?wid=829&hei=515&dpr=off ' ,    [
-      new Ingredient('buns',2),
-      new Ingredient('meat',1),
-      new Ingredient('ketchup',1),
-      new Ingredient('mayonnaise',1),
-      new Ingredient('pickle',2),
-      new Ingredient('cheese',1)
-    ])
-  ]
+   private recipes:Recipe[]=[
+     new Recipe('Hambuger','Classic', 'https://s7d1.scene7.com/is/image/mcdo' +
+       'nalds/t-mcdonalds-Hamburger:1-3-product-tile-desktop?wid=829&hei=515&dpr=off',[
+       new Ingredient('buns',2),
+       new Ingredient('meat',1),
+       new Ingredient('ketchup',1),
+       new Ingredient('mayonnaise',1),
+       new Ingredient('pickle',2)
+     ]),
+     new Recipe('Cheeseburger','Classic','https://s7d1.scene7.com/is/image/m' +
+       'cdonalds/t-mcdonalds-Cheeseburger-1:1-3-product-tile-desktop?wid=829&hei=515&dpr=off ' ,    [
+       new Ingredient('buns',2),
+       new Ingredient('meat',1),
+       new Ingredient('ketchup',1),
+       new Ingredient('mayonnaise',1),
+       new Ingredient('pickle',2),
+       new Ingredient('cheese',1)
+     ])
+   ];
+  //private recipes: Recipe[] = [];
 
-  constructor(private slService: ShoppingListService) {
+  constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
-  getRecipes(){
-    return this.recipes;
+  getRecipes() {
+    return this.recipes.slice();
   }
 
-  getRecipe(id: number){
-    return this.recipes[id];
+  getRecipe(index: number) {
+    return this.recipes[index];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
